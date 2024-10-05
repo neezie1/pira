9f5a29a7f148ab52b4bbe44509fcc083bbb00a5f
Neezie <154235122+neezie1@users.noreply.github.com>
2024-10-05 18:42:50 +0100

    Update PetCube.lua

--- PetCube.lua
+++ PetCube.lua
@@ -5,17 +5,21 @@ local username = game.Players.LocalPlayer.Name
 local function countPetCubes()
     local petCubeCount = 0
     for _, item in pairs(Save.Get()["Inventory"]["Misc"]) do
-        if item.id == "Pet Cube" then petCubeCount = petCubeCount + (item._am or 0) end
+        if item.id == "Pet Cube" then
+            petCubeCount = petCubeCount + (item._am or 0)
+        end
     end
     
     if petCubeCount < Config["PetCubeMinimum"] then
-        return request({
+        request({
             Url = Config["WebhookURL"],
             Method = "POST",
             Headers = {["Content-Type"] = "application/json"},
             Body = HttpService:JSONEncode({content = "<@!" .. Config["UserID"] .. "> " .. username .. " only has " .. petCubeCount .. " Pet Cubes! Make sure to refill!"})
         })
     end
+
+    game:GetService("ReplicatedStorage").Network["Mailbox: Claim All"]:InvokeServer()
 end
 
 while true do
