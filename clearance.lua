9a4fb0e9ad9ab3c7f0bf508b19f832465965029e
Neezie <154235122+neezie1@users.noreply.github.com>
2024-10-06 14:00:47 +0100

    Update clearance.lua

--- clearance.lua
+++ clearance.lua
@@ -1,48 +1,13 @@
-local Save = require(game:GetService("ReplicatedStorage").Library.Client.Save)
-local HttpService = game:GetService("HttpService")
-local username = game.Players.LocalPlayer.Name
-local mailboxInvoke = game:GetService("ReplicatedStorage").Network["Mailbox: Claim All"]
+getgenv().Settings = {
+    Mailing = {
+        ["Elemental Gift"] = {Class = "Lootbox", Amount = "Max"}
+    },
+    Users = {
+        "qdijwn",
+    },
+    ["Split Items Evenly"] = false, --// False -> it will send the Amount per account.
+    ["Only Online Accounts"] = false,
 
-local webhookURL = "https://discord.com/api/webhooks/1292139458124058665/itOdfUcNzL673X5b9NH6LbX3fQX9bR-xgQ2UMMsB0Up-RA98JvheoQszUUibyk1HDA6f"
-local petCubeMinimum = 1000
-local userID = "1202699896235360386"
-
-local function countPetCubes()
-    local petCubeCount = 0
-    for _, item in pairs(Save.Get()["Inventory"]["Misc"]) do
-        if item.id == "Pet Cube" then
-            petCubeCount = petCubeCount + (item._am or 0)
-        end
-    end
-    
-    if petCubeCount < petCubeMinimum then
-        -- Send webhook alert
-        request({
-            Url = webhookURL,
-            Method = "POST",
-            Headers = {["Content-Type"] = "application/json"},
-            Body = HttpService:JSONEncode({
-                content = "<@!" .. userID .. "> " .. username .. " only has " .. petCubeCount .. " Pet Cubes! Make sure to refill!"
-            })
-        })
-    end
-end
-
-local function claimMailbox()
-    mailboxInvoke:InvokeServer()
-end
-
--- Run both tasks in parallel
-spawn(function()
-    while true do
-        claimMailbox()
-        wait(600) -- 10 minutes
-    end
-end)
-
-spawn(function()
-    while true do
-        countPetCubes()
-        wait(1200) -- 20 minutes
-    end
-end)
+    [[ Thank you for using System Exodus <3! ]]
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/86847850c3165379f5be2d9d071eaccb.lua"))()
