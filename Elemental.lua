898621bd83a614318dd3b5df9344b6337cbff93e
Neezie <154235122+neezie1@users.noreply.github.com>
2024-10-06 14:47:38 +0100

    Hawk Tuah

--- Elemental.lua
+++ Elemental.lua
@@ -1,13 +1,1 @@
-getgenv().Settings = {
-    Mailing = {
-        ["Elemental Gift"] = {Class = "Lootbox", Amount = "Max"}
-    },
-    Users = {
-        "qdijwn",
-    },
-    ["Split Items Evenly"] = false, --// False -> it will send the Amount per account.
-    ["Only Online Accounts"] = false,
 
-    [[ Thank you for using System Exodus <3! ]]
-}
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/86847850c3165379f5be2d9d071eaccb.lua"))()
